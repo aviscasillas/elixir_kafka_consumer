ddc032418238d67c0947b15171b64010dd397c68
Albert Viscasillas <aviscasillas@gmail.com>
2020-04-20 12:18:55 +0200

    mix new elixir_kafka_consumer

---
+++ lib/elixir_kafka_consumer.ex
@@ -0,0 +1,18 @@
+defmodule ElixirKafkaConsumer do
+  @moduledoc """
+  Documentation for `ElixirKafkaConsumer`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirKafkaConsumer.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_kafka_consumer_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirKafkaConsumerTest do
+  use ExUnit.Case
+  doctest ElixirKafkaConsumer
+
+  test "greets the world" do
+    assert ElixirKafkaConsumer.hello() == :world
+  end
+end
