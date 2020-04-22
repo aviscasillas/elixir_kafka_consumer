6fe22c40299246c5b3854dcc6127268d20aca662
Albert Viscasillas <aviscasillas@gmail.com>
2020-04-22 14:12:49 +0200

    Fix record deletion

--- lib/elixir_kafka_consumer/generic_consumer.ex
+++ lib/elixir_kafka_consumer/generic_consumer.ex
@@ -2,7 +2,10 @@ defmodule ElixirKafkaConsumer.GenericConsumer do
   def handle_message(%{key: key, value: value}) do
     with {:ok, decoded_value} <- decode(value) do
       if decoded_value |> tombstone? do
-        ElixirKafkaConsumer.Repo.delete_all(ElixirKafkaConsumer.GenericRecord, guid: key)
+        record = ElixirKafkaConsumer.Repo.get_by(ElixirKafkaConsumer.GenericRecord, guid: key)
+        if record != nil do
+          ElixirKafkaConsumer.Repo.delete!(record)
+        end
       else
         %ElixirKafkaConsumer.GenericRecord{guid: key, body: decoded_value |> Poison.decode!}
         |> ElixirKafkaConsumer.Repo.insert(on_conflict: :replace_all, conflict_target: :guid)
